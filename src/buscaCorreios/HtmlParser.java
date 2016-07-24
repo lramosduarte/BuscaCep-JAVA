17a24bd826141336a4fb7da1f445a11c6436620e
Leonardo R Duarte <lramosduarte@gmail.com>
2016-07-24 13:47:53 -0300

    pegando os valores de label junto para realizar uma iteração conjunta e verificar qual é o atributo atual

--- src/buscaCorreios/HtmlParser.java
+++ src/buscaCorreios/HtmlParser.java
@@ -1,5 +1,9 @@
 package buscaCorreios;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -7,6 +11,8 @@ import java.util.regex.Pattern;
  * Created by lramosduarte on 24/07/16.
  */
 public class HtmlParser {
+    private final static Pattern TAG_NOMES = Pattern.compile("<span " +
+            "class=\"resposta\">(.+?)</span>");
     private final static Pattern TAG = Pattern.compile("<span " +
             "class=\"respostadestaque\">(.+?)</span>");
 
@@ -17,10 +23,27 @@ public class HtmlParser {
     }
 
     public Dados parser(StringBuffer html){
-        Matcher regex = TAG.matcher(html);
-        while(regex.find()){
-            extrairDados(regex.group(1));
+        Iterator<String> nomes = nomeTags(html).iterator();
+        Iterator<String> valores = valoresTags(html).iterator();
+        while(nomes.hasNext() && valores.hasNext()){
+            
         }
         return dados;
     }
-}
+
+    public List nomeTags(StringBuffer html){
+        List<String> nomes = new ArrayList<>();
+        Matcher regex = TAG_NOMES.matcher(html);
+        while (regex.find())
+            nomes.add(regex.group(1));
+        return nomes;
+    }
+
+    public List valoresTags(StringBuffer html){
+        List<String> valores = new ArrayList<>();
+        Matcher regex = TAG.matcher(html);
+        while (regex.find())
+            valores.add(regex.group(1));
+        return valores;
+    }
+ }
