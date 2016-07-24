047bfb886df86133cd3a42e6f1c302d4a5ba0a46
Leonardo R Duarte <lramosduarte@gmail.com>
2016-07-24 13:05:57 -0300

    refatorado

--- src/buscaCorreios/Dados.java
+++ src/buscaCorreios/Dados.java
@@ -15,30 +15,4 @@ public class Dados {
         cidade = logradouro.split("/")[0];
         estado = logradouro.split("/")[1];
     }
-
-    public int getCep() {
-        return cep;
-    }
-
-    public void setCep(int cep) {
-        this.cep = cep;
-    }
-
-    public String getBairro() {
-        return bairro;
-    }
-
-    public void setBairro(String bairro) {
-        this.bairro = bairro;
-    }
-
-    public String getLocalidade() {
-        return localidade;
-    }
-
-    public void setLocalidade(String localidade) {
-        this.localidade = localidade;
-    }
-
-
 }
