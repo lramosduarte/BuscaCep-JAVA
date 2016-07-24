53011408e6dc99f54e3586cc123c91c994e81d64
Leonardo R Duarte <lramosduarte@gmail.com>
2016-07-24 13:22:05 -0300

    faz parser do html para o model

--- src/buscaCorreios/HtmlParser.java
+++ src/buscaCorreios/HtmlParser.java
@@ -1,7 +1,26 @@
 package buscaCorreios;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by lramosduarte on 24/07/16.
  */
 public class HtmlParser {
+    private final static Pattern TAG = Pattern.compile("<span " +
+            "class=\"respostadestaque\">(.+?)</span>");
+
+    private Dados dados;
+
+    public HtmlParser() {
+        this.dados = new Dados();
+    }
+
+    public Dados parser(StringBuffer html){
+        Matcher regex = TAG.matcher(html);
+        while(regex.find()){
+            extrairDados(regex.group(1));
+        }
+        return dados;
+    }
 }
